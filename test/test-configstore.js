952f494165796cbd766d25989a4886068722add7
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-22 11:40:52 +0100

    /lib dir is antipattern

--- test/test-configstore.js
+++ test/test-configstore.js
@@ -2,43 +2,43 @@
 'use strict';
 var assert = require('assert');
 var fs = require('fs');
-var Configstore = require('../lib/configstore');
+var Configstore = require('../configstore');
 
-describe('configstore', function() {
+describe('configstore', function () {
 	var configstorePath = new Configstore('configstore-test').path;
 
-	beforeEach(function() {
+	beforeEach(function () {
 		fs.unlinkSync(configstorePath);
 		this.conf = new Configstore('configstore-test');
 	});
 
-	it('.set() and .get()', function() {
+	it('.set() and .get()', function () {
 		this.conf.set('foo', 'bar');
 		assert.equal(this.conf.get('foo'), 'bar');
 	});
 
-	it('.del()', function() {
+	it('.del()', function () {
 		this.conf.set('foo', 'bar');
 		this.conf.del('foo');
 		assert.notEqual(this.conf.get('foo'), 'bar');
 	});
 
-	it('.all', function() {
+	it('.all', function () {
 		this.conf.set('foo', 'bar');
 		assert.equal(this.conf.all.foo, 'bar');
 	});
 
-	it('.size', function() {
+	it('.size', function () {
 		this.conf.set('foo', 'bar');
 		assert.equal(this.conf.size, 1);
 	});
 
-	it('.path', function() {
+	it('.path', function () {
 		this.conf.set('foo', 'bar');
 		assert(fs.existsSync(this.conf.path));
 	});
 
-	it('should use default value', function() {
+	it('should use default value', function () {
 		var conf = new Configstore('configstore-test', { foo: 'bar' });
 		assert.equal(conf.get('foo'), 'bar');
 	});
