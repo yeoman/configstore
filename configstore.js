bc9d11b73ed75d0a062e42fd48251d1ddb665643
Sindre Sorhus <sindresorhus@gmail.com>
2013-06-29 01:03:24 +0200

    Switch to js-yaml and improve error handling
    Switching to it since it's much more popular, faster and safer.
    
    Also only allow JSON types.

--- configstore.js
+++ configstore.js
@@ -1,28 +1,17 @@
 'use strict';
 var path = require('path');
 var fs = require('fs');
+var EOL = require('os').EOL;
 var _ = require('lodash');
 var mkdirp = require('mkdirp');
-var YAML = require('yamljs');
+var yaml = require('js-yaml');
 
 var configDir = process.env.XDG_CONFIG_HOME ||
 	path.join(process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'],
 		'.config');
 
-
-function readFile(filePath) {
-	try {
-		return fs.readFileSync(filePath, 'utf8');
-	} catch (err) {
-		// Create dir if it doesn't exist
-		if (err.code === 'ENOENT') {
-			mkdirp.sync(path.dirname(filePath));
-			return '';
-		}
-
-		// Rethrow the error
-		throw err;
-	}
+function permissionError() {
+	return 'You don\'t have access to this file.';
 }
 
 function Configstore(id, defaults) {
@@ -33,10 +22,40 @@ function Configstore(id, defaults) {
 Configstore.prototype = Object.create(Object.prototype, {
 	all: {
 		get: function () {
-			return YAML.parse(readFile(this.path)) || {};
+			try {
+				return yaml.safeLoad(fs.readFileSync(this.path, 'utf8'), {
+					filename: this.path,
+					schema: yaml.JSON_SCHEMA
+				});
+			} catch (err) {
+				// create dir if it doesn't exist
+				if (err.code === 'ENOENT') {
+					mkdirp.sync(path.dirname(this.path));
+					return;
+				}
+
+				// improve the message of permission errors
+				if (err.code === 'EACCES') {
+					err.message = err.message + EOL + permissionError() + EOL;
+				}
+
+				throw err;
+			}
 		},
 		set: function (val) {
-			fs.writeFileSync(this.path, YAML.stringify(val));
+			try {
+				fs.writeFileSync(this.path, yaml.safeDump(val, {
+					skipInvalid: true,
+					schema: yaml.JSON_SCHEMA
+				}));
+			} catch (err) {
+				// improve the message of permission errors
+				if (err.code === 'EACCES') {
+					err.message = err.message + EOL + permissionError() + EOL;
+				}
+
+				throw err;
+			}
 		}
 	},
 	size: {
