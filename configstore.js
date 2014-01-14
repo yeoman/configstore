962977c648451baa7f87be06203926d41fb5039d
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-14 13:51:44 +0100

    various tweaks and drop node 0.8 support

--- configstore.js
+++ configstore.js
@@ -1,23 +1,17 @@
 'use strict';
 var path = require('path');
-var fs = require('graceful-fs');
-var crypto = require('crypto');
 var os = require('os');
+var fs = require('graceful-fs');
 var osenv = require('osenv');
-var EOL = os.EOL;
 var _ = require('lodash');
 var mkdirp = require('mkdirp');
 var yaml = require('js-yaml');
+var uuid = require('uuid');
 
-var user = (osenv.user() || generateFakeUser()).replace(/\\/g, '-');
-var tmpDir = path.join(os.tmpdir ? os.tmpdir() : os.tmpDir(), user);
+var user = (osenv.user() || uuid.v4()).replace(/\\/g, '');
+var tmpDir = path.join(os.tmpdir(), user);
 var configDir = process.env.XDG_CONFIG_HOME || path.join(osenv.home() || tmpDir, '.config');
 var permissionError = 'You don\'t have access to this file.';
-
-function generateFakeUser() {
-	var uid = [process.pid, Date.now(), Math.floor(Math.random() * 1000000)].join('-');
-	return crypto.createHash('md5').update(uid).digest('hex');
-}
 
 function Configstore(id, defaults) {
 	this.path = path.join(configDir, 'configstore', id + '.yml');
@@ -41,7 +35,7 @@ Configstore.prototype = Object.create(Object.prototype, {
 
 				// improve the message of permission errors
 				if (err.code === 'EACCES') {
-					err.message = err.message + EOL + permissionError + EOL;
+					err.message = err.message + '\n' + permissionError + '\n';
 				}
 
 				// empty the file if it encounters invalid YAML
@@ -65,7 +59,7 @@ Configstore.prototype = Object.create(Object.prototype, {
 			} catch (err) {
 				// improve the message of permission errors
 				if (err.code === 'EACCES') {
-					err.message = err.message + EOL + permissionError + EOL;
+					err.message = err.message + '\n' + permissionError + '\n';
 				}
 
 				throw err;
